a057e5b5ebd0edc402ade69f10a4ace1d1ab4e5c
Noah <100643509+noneill256@users.noreply.github.com>
2022-07-05 10:38:01 -0700

    Update fridge.py

--- fridge.py
+++ fridge.py
@@ -1,8 +1,10 @@
 # input the ingredients into a list
 fridge = []
 ingred = ''
-while ingred.upper() != 'Q':
-  ingred = input("\nEnter an ingredient. Press 'Q' when finished.")
-  if ingred.upper() == 'Q':
-    break
-  fridge.append(ingred)
+
+def enter_ingredients():
+  while ingred.upper() != 'Q':
+    ingred = input("\nEnter an ingredient. Press 'Q' when finished.")
+    if ingred.upper() == 'Q':
+      break
+    fridge.append(ingred)
